3aa7bbcc990d744400edae5747174bf8ab2e1cb3
SHUBHAM KUMAR JAIN <38971147+shubhamkrjain@users.noreply.github.com>
2020-06-15 18:10:41 +0530

    kjbwfkj

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html>
 <body>
-<h1>HARRY POTTER</h1>
-<h3>lets have fun</h3>
+<h1>This is my linkedin profile</h1>
+   <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="shubham-kumar-jain-533010163"><a class="LI-simple-link" href='https://in.linkedin.com/in/shubham-kumar-jain-533010163?trk=profile-badge'>SHUBHAM KUMAR JAIN</a></div>
+<h3>And my picture</h3>
 <?php
    $cloudant_url=`head -n1 path.txt`;
    $img_path="https://".$cloudant_url."/terraform.png";
